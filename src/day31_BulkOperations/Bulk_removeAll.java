290fb3af21b841de3e4a52151bba38536edac676
cybertekschool <github@cybertekschool.com>
2020-04-30 12:03:59 -0400

    Day31 Class

---
+++ src/day31_BulkOperations/Bulk_removeAll.java
@@ -0,0 +1,20 @@
+package day31_BulkOperations;
+
+
+import java.util.ArrayList;
+
+public class Bulk_removeAll {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        /*
+             1. write a program that can revome duplkcates from an arraylist
+
+         */
+
+
+    }
+
+}
