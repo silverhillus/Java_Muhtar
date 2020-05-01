a8aa0b32c86dd6285bdf97f7bf0f389cc0314967
cybertekschool <github@cybertekschool.com>
2020-05-01 16:16:21 -0400

    Day32 Class

---
+++ src/day32_Predicate/Quiz.java
@@ -0,0 +1,10 @@
+package day32_Predicate;
+
+public class Quiz {
+
+    /*
+    1. write a program that can remove all the special characters from a list of Character
+
+    2. write a program that can remove all the numbers that can divisble by 3 or 5 in arrayList
+     */
+}
